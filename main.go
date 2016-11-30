fe473531c2d419456e2f636207da0602f094b345
Chiheb NeXus <chihebnexus@gmail.com>
2016-11-30 19:12:12 +0100

    Build passing

--- main.go
+++ main.go
@@ -2,35 +2,35 @@ package main
 
 import (
 	"fmt"
-	"blockr.io/api"
+	"blockrio/api"
 )
 
 func main() {
-	
-/*
-	Get multiple addresses balance.
-	Method: 
-		- Using: GetAddressBalance(coin string, add []string) -> return []ResponseAddressBalance
-		- Extracted data are: 
-			* Status -> string
-			* Data -> ResponseAddressBalance.[]_DataBalance{
-											Address -> string
-											Balance -> float64
-									BalanceMultiSig -> float64
-											}
-				* Code -> float64
-				* Message -> string
-		So, in order to get the addresses's balance we should range at the retern values of GetAddressBalance
-			Example: v := GetAddressBalance(coin, add)
-		Then range second time at the value of v.Data using "val" as key. 
-		Finally we can retrieve Address and Balance with: 
-			Address: val.Address
-			Balance: val.Balance
+
+	/*
+		Get multiple addresses balance.
+		Method:
+			- Using: GetAddressBalance(coin string, add []string) -> return []ResponseAddressBalance
+			- Extracted data are:
+				* Status -> string
+				* Data -> ResponseAddressBalance.[]_DataBalance{
+												Address -> string
+												Balance -> float64
+										BalanceMultiSig -> float64
+												}
+					* Code -> float64
+					* Message -> string
+			So, in order to get the addresses's balance we should range at the retern values of GetAddressBalance
+				Example: v := GetAddressBalance(coin, add)
+			Then range second time at the value of v.Data using "val" as key.
+			Finally we can retrieve Address and Balance with:
+				Address: val.Address
+				Balance: val.Balance
 	*/
 	add := []string{
 		"198aMn6ZYAczwrE5NvNTUMyJ5qkfy4g3Hi",
 		"1L8meqhMTRpxasdGt8DHSJfscxgHHzvPgk",
-		}
+	}
 	coin := "btc"
 	address_balance := blockrio.GetAddressBalance(coin, add)
 	for _, value := range address_balance {
@@ -39,4 +39,3 @@ func main() {
 		}
 	}
 }
-		
